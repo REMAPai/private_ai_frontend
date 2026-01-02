77dcc266a38426cca75b978e1f10f89bca630d61
arham-revring <i.am.arhamkhan25@gmail.com>
2026-01-02 19:31:38 +0500

    Update Sidebar.svelte

--- src/lib/components/layout/Sidebar.svelte
+++ src/lib/components/layout/Sidebar.svelte
@@ -817,7 +817,8 @@
 
 				<a href="/" class="flex flex-1 px-1.5" on:click={newChatHandler}>
 					<div
-						id="sidebar-webui-name" id="sidebar-workspace"
+						id="sidebar-webui-name" 
+						  data-section="sidebar-workspace"
 						class=" self-center font-medium text-gray-850 dark:text-white font-primary"
 					>
 						{$WEBUI_NAME}
